3e9d39a82c5bc2fb88c28b5854d50d1621bb9821
bwissal13 <baazizwissal13@gmail.com>
2024-08-30 16:30:35 +0100

    add Hotel class

---
+++ src/Hotel.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Hotel {
+    private ArrayList<Room> rooms = new ArrayList<>();
+    private ArrayList<Reservation> reservations = new ArrayList<>();
+
+}
